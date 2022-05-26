d103a9ac9ce3d0bfcae9cecb29d2a1e6124d5e08
Lftobs <106287048+Lftobs@users.noreply.github.com>
2022-05-26 01:41:50 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+        
+   def __init__(self, name, age, track, score):
+            self.name = str(name)
+            self.age = int(age)
+            self.track = str(track)
+            self.score = str(score)
+            
+   def change_name(self, c_n):
+          self.name = str(c_n)
+        
+   def change_age(self, c_age):
+   	   self.age = int(c_age)
+   	   
+   def add_track(self, a_track):
+   	   self.track = str(a_track)
+   	  
+   
+   def get_score(score =20.90 ):
+   	   return score	   	   
+
+
+Bob = Student("Bob", 26, ["FE","BE"], 20.90)
+''' overriden objects
+print(Bob.name)
+print(Bob.age)
+print(Bob.track)
+print(Bob.score)
+'''
+
+
+# Expected methods
+Bob.change_name("peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
+
+print(Bob.name)
+print(Bob.age)
+print(Bob.track)
+print(Bob.score)
